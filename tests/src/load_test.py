db2b90f79292a256b5c8fff8476ccc36bb364d3f
Stijn Kramer <16038058+Stijn-K@users.noreply.github.com>
2022-04-23 20:34:59 +0200

    fixed error causing malformed requests

--- tests/src/load_test.py
+++ tests/src/load_test.py
@@ -15,7 +15,9 @@ load_dotenv()
 
 _SERVER = os.getenv('SERVER')
 _URL = f'https://{_SERVER}:5000'
+
 _ERROR_RE = re.compile(r'<h2 id="error">Error: (.*?)<\/h2>', re.DOTALL)
+
 
 class TestUser(FastHttpUser):
     wait_time = constant_throughput(1)
@@ -43,7 +45,7 @@ class TestUser(FastHttpUser):
     }
 
     def on_start(self):
-        with self.client.post(f'{_URL}/login', json=self.data, verify=False) as response:
+        with self.client.post(f'{_URL}/login', json=self.data) as response:
             otc_values = response.headers['X-OTC-SET'].split(',')
             self._OTC['cid'] = otc_values[0]
             self._OTC['domain'] = otc_values[1]
@@ -62,7 +64,7 @@ class TestUser(FastHttpUser):
         h = hmac.new(bytes.fromhex(self._OTC['k_s']), msg, hashlib.sha256).hexdigest()
         otc_header = f'{self._OTC["cid"]},{self._OTC["n_s"]},{t_h},{h},{self._OTC["ticket"]}'
 
-        with self.client.get(f'{_URL}/user', verify=False, headers={'X-OTC': otc_header}, catch_response=True) as response:
+        with self.client.get(f'{_URL}/user', headers={'X-OTC': otc_header}, catch_response=True) as response:
             if error := _ERROR_RE.search(response.text):
                 response.failure(error.group(1))
 
