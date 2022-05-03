53f9920fa2d8e1566ac99f5e1ba794804fbdcdb1
Stijn Kramer <16038058+Stijn-K@users.noreply.github.com>
2022-05-03 20:23:07 +0200

    adjust num runs

--- tests/src/client_side_tests.py
+++ tests/src/client_side_tests.py
@@ -125,7 +125,7 @@ def write_to_file(data):
 
 
 if __name__ == '__main__':
-    num_tests = 100
+    num_tests = 110
 
     options = webdriver.ChromeOptions()
     # ignore errors caused by self-signed certificates
