3643944de0737f56368e650b9c0841008d591e51
Stijn Kramer <16038058+Stijn-K@users.noreply.github.com>
2022-05-03 20:43:33 +0200

    write results to file; added numpy to pipfile

--- tests/src/client_side_tests.py
+++ tests/src/client_side_tests.py
@@ -1,6 +1,7 @@
 import os
 import time
 import sys
+import json
 
 from seleniumwire import webdriver
 from seleniumwire.request import Request, Response
@@ -22,6 +23,9 @@ load_dotenv()
 _DRIVER_PATH = os.getenv('CHROMEDRIVER')
 _SERVER = os.getenv('SERVER')
 _URL = f'https://{_SERVER}:5000'
+
+_RESULTS_PATH = os.getenv('RESULTS')
+_BRANCH = os.getenv('BRANCH')
 
 timings = {
     'login': {
@@ -119,8 +123,13 @@ def time_user(driver: WebDriver) -> None:
     timings['user']['backend'].append(backend_performance)
 
 
+def write_to_file(data):
+    with open(f'{_RESULTS_PATH}timings-{_BRANCH}.json', 'w+') as f:
+        f.write(json.dumps(data))
+
+
 if __name__ == '__main__':
-    num_tests = 5
+    num_tests = 110
 
     np.random.seed(0)
     _NUM_PUFS = 5
@@ -168,3 +177,4 @@ if __name__ == '__main__':
     driver.quit()
 
     print(timings)
+    write_to_file(timings)
