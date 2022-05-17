567137f92f566dc46e98be96a2a93a9c3eed1663
Stijn Kramer <16038058+Stijn-K@users.noreply.github.com>
2022-05-17 21:22:04 +0200

    load tests now runnable from script

--- tests/src/load_test.py
+++ tests/src/load_test.py
@@ -7,13 +7,18 @@ from contextlib import contextmanager
 import warnings
 import urllib3
 
-from locust import FastHttpUser, task, constant_throughput, HttpUser
+import gevent
+from locust import HttpUser, task, constant_throughput
+from locust.env import Environment
+from locust.stats import stats_history, StatsCSVFileWriter
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 _SERVER = os.getenv('SERVER')
+_RESULTS = os.getenv("RESULTS")
+_BRANCH = os.getenv("BRANCH")
 _URL = f'https://{_SERVER}:5000'
 _CLIENT_CERT = os.getenv('CLIENT_CERT')
 
@@ -46,3 +51,42 @@ class TestUser(HttpUser):
             with self.client.get(f'{_URL}/user', cert=_CLIENT_CERT, verify=False, catch_response=True) as response:
                 if error := _ERROR_RE.search(response.text):
                     response.failure(error.group(1))
+
+
+def start_locust(users: int, spawn_rate: int, time_min: int) -> None:
+    # Setup Environment and Runner
+    env = Environment(user_classes=[TestUser])
+    env.create_local_runner()
+
+    # CSV Writer
+    stats_path = os.path.join(_RESULTS, 'load_tests', _BRANCH, f'{users}')
+    csv_writer = StatsCSVFileWriter(
+        environment=env,
+        base_filepath=stats_path,
+        full_history=False,
+        percentiles_to_report=[0.50, 0.95]
+    )
+
+    # Start test
+    env.runner.start(users, spawn_rate)
+
+    # start a greenlet to save stats to history
+    gevent.spawn(stats_history, env.runner)
+
+    # stop the runner after time_min minutes
+    gevent.spawn_later(time_min * 60, lambda: env.runner.quit())
+
+    # start a greenlet to write stats to csv
+    gevent.spawn(csv_writer.stats_writer)
+
+    # wait for all greenlets to finish
+    env.runner.greenlet.join()
+
+
+if __name__ == '__main__':
+    tests = [(5, 1, 1), (10, 2, 1), (20, 5, 1), (50, 10, 1), (100, 20, 1)]
+    num_tests = len(tests)
+    for num, (users, spawn_rate, time_min) in enumerate(tests, 1):
+        print(f'{num}/{num_tests}: Running test with {users} users, {spawn_rate} spawn rate, {time_min} minutes')
+        start_locust(users, spawn_rate, time_min)
+        print(f'Test finished')
