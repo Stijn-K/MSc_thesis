941de2dbca9c03f05364bafeb32ead4e25809811
Stijn Kramer <16038058+Stijn-K@users.noreply.github.com>
2022-06-15 17:59:17 +0200

    updates

--- tests/src/load_test.py
+++ tests/src/load_test.py
@@ -69,7 +69,7 @@ def start_locust(users: int, spawn_rate: int, time_min: int, stats_path: str) ->
 
 
 if __name__ == '__main__':
-    tests = [(1, 5, .5), (2, 5, .5), (5, 5, .5), (10, 5, .5), (20, 5, .5), (40, 10, .5), (80, 20, .5), (160, 40, .75)]
+    tests = [(1, 5, 1), (2, 5, 1), (5, 5, 1), (10, 5, 1), (20, 5, 1), (50, 10, 1), (100, 20, 1)]
     num_tests = len(tests)
 
     path = os.path.join(_RESULTS, 'load_tests', _BRANCH)
